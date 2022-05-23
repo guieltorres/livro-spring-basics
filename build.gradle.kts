3b18d23e4ebf244b90240c76e2e35491af2cb861
Guilherme Torres <guilherme.torres1@creditas.com>
2022-05-23 16:47:54 -0300

    refactor application to postgres database and add flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.creditas"
@@ -22,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.postgresql:postgresql")
+	implementation("org.flywaydb:flyway-core:8.5.11")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
