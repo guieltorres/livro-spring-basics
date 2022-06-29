fcdd980d7dbe897ff55bd34d46cd1e01f5bc9a52
Guilherme Torres <guilherme.torres1@creditas.com>
2022-06-29 16:49:40 -0300

    add spring profiles

--- src/main/kotlin/com/creditas/livro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/creditas/livro/config/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.creditas.livro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -10,6 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@Profile("!prod")
 @EnableSwagger2
 class SwaggerConfig {
 
