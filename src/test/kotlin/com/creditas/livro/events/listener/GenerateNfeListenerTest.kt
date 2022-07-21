398b8e340026ee363813d8260b5ad5ffe1fa14ad
Guilherme Torres <guilherme.torres1@creditas.com>
2022-07-21 11:01:56 -0300

    implemented generate nfe listener tests

---
+++ src/test/kotlin/com/creditas/livro/events/listener/GenerateNfeListenerTest.kt
@@ -0,0 +1,45 @@
+package com.creditas.livro.events.listener
+
+import com.creditas.livro.events.PurchaseEvent
+import com.creditas.livro.helper.buildPurchase
+import com.creditas.livro.service.PurchaseService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.mockkStatic
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+
+
+@ExtendWith(MockKExtension::class)
+class GenerateNfeListenerTest {
+
+    @MockK
+    private lateinit var purchaseService: PurchaseService
+
+    @InjectMockKs
+    private lateinit var generateNfeListener: GenerateNfeListener
+
+    @Test
+    fun `should generate nfe`(){
+
+        val purchase = buildPurchase(nfe = null)
+        val fakeNfe = UUID.randomUUID()
+        val purchaseExpected = purchase.copy(nfe = fakeNfe.toString())
+        mockkStatic(UUID::class)
+
+        every { UUID.randomUUID() } returns fakeNfe
+        every { purchaseService.update(purchaseExpected) } just runs
+
+        generateNfeListener.listen(PurchaseEvent(this, purchase))
+
+        verify (exactly = 1){ purchaseService.update(purchaseExpected) }
+    }
+
+}
